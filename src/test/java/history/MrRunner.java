bf7685675ce4a8df3769aacfbc4df8d94d2f80fc
chandhuhimakar <54269183+chandhuhimakar@users.noreply.github.com>
2019-08-21 14:01:09 +0530

    Update MrRunner.java

--- src/test/java/history/MrRunner.java
+++ src/test/java/history/MrRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="MyFeatures",glue="history",plugin= {"html:target/cucumber-htmlreporter.html","json:target/cucumber.json"})
+@CucumberOptions(features="MyFeatures",glue="history",plugin= {"json:target/cucumber.json"})
 
 public class MrRunner {
 	
